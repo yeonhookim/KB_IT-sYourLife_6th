e6d8f8c2ed0f378043782dac9d39c138ab42880c
yeonhookim <rladusgn68@gmail.com>
2025-05-21 14:20:42 +0900

    2025_05_21v2

---
+++ jdbc_ex/src/test/java/org/scoula/jdbc_ex/common/JDBCUtilTest.java
@@ -0,0 +1,13 @@
+package org.scoula.jdbc_ex.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+// ctrl+shift+T
+class JDBCUtilTest {
+
+    @Test
+    void getConnection() {
+
+    }
+}
