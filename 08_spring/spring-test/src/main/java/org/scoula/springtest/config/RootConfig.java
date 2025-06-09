061aef75bcef33c2d9aa174d6f18489b360ae609
yeonhookim <rladusgn68@gmail.com>
2025-06-09 17:47:44 +0900

    서브모듈 제거 및 내부 코드 푸시

---
+++ 08_spring/spring-test/src/main/java/org/scoula/springtest/config/RootConfig.java
@@ -0,0 +1,8 @@
+package org.scoula.springtest.config;
+
+import org.springframework.context.annotation.Configuration;
+// Configuration를 build.gradle에서 등록을 했기 때문에 가져올 수 있음
+
+@Configuration
+public class RootConfig {
+}
