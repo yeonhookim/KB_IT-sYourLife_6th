389d37758e5b2a908a4c3ba3f51e6b88032168cc
yeonhookim <rladusgn68@gmail.com>
2025-06-09 18:50:53 +0900

    2025_06_09v2

--- 06_Jsp&Sevlet/ex02/src/main/java/org/scoula/ex02/HelloServlet.java
+++ 06_Jsp&Sevlet/ex02/src/main/java/org/scoula/ex02/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private String message;
     public void init() {
-        message = "Hello World!";
+        message = "Hello World!!";
     }
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         resp.setContentType("text/html");
